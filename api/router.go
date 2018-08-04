79267c8b879c19bcca76918c5f9690c6d8a27ec1
Tomokazu Tantaka <tantaka.tomokazu@gmail.com>
2018-08-04 11:51:07 +0900

    initial commit

---
+++ api/router.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/jinzhu/gorm"
+	"github.com/julienschmidt/httprouter"
+)
+
+// example for init the database:
+//
+//  DB, err := gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/employees?charset=utf8&parseTime=true")
+//  if err != nil {
+//  	panic("failed to connect database: " + err.Error())
+//  }
+//  defer db.Close()
+
+var DB *gorm.DB
+
+func ConfigRouter() http.Handler {
+	router := httprouter.New()
+	configActorsRouter(router)
+	configAddressesRouter(router)
+	configCategoriesRouter(router)
+	configCitiesRouter(router)
+	configCountriesRouter(router)
+	configCustomersRouter(router)
+	configFilmsRouter(router)
+	configFilmActorsRouter(router)
+	configFilmCategoriesRouter(router)
+	configFilmTextsRouter(router)
+	configInventoriesRouter(router)
+	configLanguagesRouter(router)
+	configPaymentsRouter(router)
+	configRentalsRouter(router)
+	configStaffsRouter(router)
+	configStoresRouter(router)
+
+	return router
+}
+
+func writeJSON(w http.ResponseWriter, v interface{}) {
+	data, _ := json.Marshal(v)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Write(data)
+}
+
+func readJSON(r *http.Request, v interface{}) error {
+	buf, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(buf, v)
+}
